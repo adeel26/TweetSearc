1d05cd47abde8a7a844328f06953ca0e624d315f
Malik Adeel <malikadeel@Maliks-MacBook-Pro.local>
2013-09-29 13:46:05 +0100

    Initial Commit

---
+++ TweetSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetSearch
+//
+//  Created by Malik Adeel on 29/09/2013.
+//  Copyright (c) 2013 Malik Adeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TweetSearch
+//
+//  Created by Malik Adeel on 29/09/2013.
+//  Copyright (c) 2013 Malik Adeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
